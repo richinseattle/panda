e0e84a943f6fc119344acb6b36267209a4a448d4
Manolis Stamatogiannakis <manolis.stamatogiannakis@vu.nl>
2015-02-17 13:21:26 +0100

    debug and log macros

--- qemu/panda_plugins/osi/osi_proc_events.h
+++ qemu/panda_plugins/osi/osi_proc_events.h
@@ -15,9 +15,28 @@ extern "C" {
 
 /*!
  * @brief Branch prediction hint macros.
-*/
+ */
 #define likely(x)       __builtin_expect(!!(x), 1)
 #define unlikely(x)     __builtin_expect(!!(x), 0)
+
+/*!
+ * @brief Debug macros.
+ */
+#define LOG_ERROR(fmt, args...) fprintf(stderr, "ERROR(%s:%s): " fmt "\n", __FILE__, __func__, ## args)
+#define LOG_WARN(fmt, args...) fprintf(stderr, "WARN(%s:%s): " fmt "\n", __FILE__, __func__, ## args)
+#define LOG_INFO(fmt, args...) fprintf(stderr, "INFO(%s:%s): " fmt "\n", __FILE__, __func__, ## args)
+#define PRINT_CONTAINER(c, fmt, args...) do{\
+	int _l = 0;\
+	LOG_INFO("------- " #c " start -------");\
+	for (auto it=(c)->begin(); it!=(c)->end(); ++it){\
+		char _d = _l ? ' ' : '\t';\
+		fprintf(stderr, "%c" fmt, _d, ## args);\
+		_l = (_l + 1) % 10;\
+		if (!_l) fprintf(stderr, "\n");\
+	}\
+	if (_l) fprintf(stderr, "\n");\
+	LOG_INFO("-------- " #c " end --------\n");\
+} while(0)
 
 #ifdef __cplusplus
 #include <set>
