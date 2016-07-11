5d329474ccce9ef88df8e163ba35852a4c800aed
Manolis Stamatogiannakis <mstamat@gmail.com>
2016-07-11 11:11:52 +0200

    build.sh now uses make to generate pandalog.proto. build.sh avoids redundant re-configuration when run more than once. config-host.mak has to be removed to force reconfiguration.
    TODO: Call of apigen.py still forces un-needed recompilations of some files.

--- qemu/build.sh
+++ qemu/build.sh
@@ -5,16 +5,10 @@ PANDA_LLVM_ROOT="${PANDA_LLVM_ROOT:-../llvm}"
 PANDA_LLVM_BUILD="${PANDA_LLVM_BUILD:-Release}"
 PANDA_LLVM="$(/bin/readlink -f "${PANDA_LLVM_ROOT}/${PANDA_LLVM_BUILD}" 2>/dev/null)"
 
-# stop on any error
+# Stop on any error after this point.
 set -e
 
-# creates api code for plugins
-python ../scripts/apigen.py
-
-# creates pandalog code
-sh ./pp.sh
-
-# set the LLVM_BIT
+# Set the LLVM_BIT.
 if [ "$PANDA_LLVM" != "" ]; then
   # Using PANDA LLVM.
   echo "Found PANDA LLVM on ${PANDA_LLVM_ROOT} -- LLVM SUPPORT IS ENABLED"
@@ -33,14 +27,27 @@ else
   fi
 fi  
 
-./configure --target-list=x86_64-softmmu,i386-softmmu,arm-softmmu \
---prefix=`pwd`/install \
---disable-pie \
---disable-xen \
---disable-libiscsi \
-$LLVM_BIT \
---extra-cflags="-O2 -I/usr/local/include" \
---extra-cxxflags="-O2" \
---extra-ldflags="-L/usr/local/lib -L/usr/local/lib64 -lprotobuf-c -lprotobuf -lpthread"
+# create api code for plugins
+python ../scripts/apigen.py
 
+# create pandalog code
+make -C panda
+
+# configure - if needed
+if [ ! -f config-host.mak ]; then
+  echo "No config-host.mak. Running configure script."
+  ./configure --target-list=x86_64-softmmu,i386-softmmu,arm-softmmu \
+    --prefix=`pwd`/install \
+    --disable-pie \
+    --disable-xen \
+    --disable-libiscsi \
+    $LLVM_BIT \
+    --extra-cflags="-O2 -I/usr/local/include" \
+    --extra-cxxflags="-O2" \
+    --extra-ldflags="-L/usr/local/lib -L/usr/local/lib64 -lprotobuf-c -lprotobuf -lpthread"
+else
+  echo "Found config-host.mak. Not running configure script."
+fi
+
+# compile
 make -j ${PANDA_NPROC:-$(nproc)}
